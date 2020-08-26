b3562264e563a8d645585c444a71580924a3648c
NathanCheshire <nathan.vincent.2.718@gmail.com>
2020-08-26 17:58:26 -0500

    code test commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] ignored) {
+        JOptionPane.showMessageDialog(null,
+                "<html>An email client by <br/>Mallory Duke and<br/>Nathan Cheshire</html>",
+                "Straight Shot", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
