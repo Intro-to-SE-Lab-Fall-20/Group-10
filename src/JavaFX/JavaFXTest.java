061ae19ec117fb909799cfa0c6ef4d2b8e19dc81
Mallory Duke <69919745+malloryDuke@users.noreply.github.com>
2020-08-27 18:02:38 -0500

    changed login screen

--- src/JavaFX/JavaFXTest.java
+++ src/JavaFX/JavaFXTest.java
@@ -2,20 +2,25 @@ package JavaFX;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import javafx.scene.image.Image;
+
+import java.awt.*;
 
 
 public class JavaFXTest extends Application {
     //todo change css to style wanted
-    private static String username;
-    private String password;
+    private PasswordField passwordText;
 
     public static void main(String[] args) {
         launch(args);
@@ -33,26 +38,42 @@ public class JavaFXTest extends Application {
         Label usernameLabel = new Label("username: ");
         Label passwordLabel = new Label("password: ");
         TextField usernameText = new TextField();
-        PasswordField passwordText = new PasswordField();
+        passwordText = new PasswordField();
         Button login = new Button("login");
-        login.setOnAction(e-> verifyPassword(passwordText.getText()));
+        login.setOnAction(e-> verifyPassword());
         login.getStyleClass().add("button-blue");
-        ProgressBar pb = new ProgressBar();
-        pb.setPrefSize(200,30);
+        login.setTooltip(new Tooltip("press to login"));
 
-        GridPane.setConstraints(usernameLabel, 0,0);
-        GridPane.setConstraints(passwordLabel, 0,1);
-        GridPane.setConstraints(usernameText, 1, 0);
-        GridPane.setConstraints(passwordText, 1, 1);
-        GridPane.setConstraints(login, 1, 2);
-        GridPane.setConstraints(pb, 1,4);
+        Button switchAccount = new Button("switch/logout");
 
-        gridPane.getChildren().addAll(usernameLabel,  passwordLabel, usernameText, passwordText, login, pb);
+        HBox buttonHbox = new HBox(5);
+        buttonHbox.getChildren().addAll(login, switchAccount);
+        //ProgressBar pb = new ProgressBar();
+        //pb.setPrefSize(200,30);
+
+        GridPane.setConstraints(usernameLabel, 0,1);
+        GridPane.setConstraints(passwordLabel, 0,3);
+        GridPane.setConstraints(usernameText, 0, 2);
+        GridPane.setConstraints(passwordText, 0, 4);
+        //GridPane.setConstraints(login, 0, 5);
+        GridPane.setConstraints(buttonHbox, 0, 5);
+        //GridPane.setConstraints(pb, 1,4);
+
+        ChoiceBox<String> switchChoice = new ChoiceBox<>();
+        switchChoice.getItems().add("Switch");
+        switchChoice.getItems().add("Logout");
+
+        // todo put in switch account/logout button
+
+        gridPane.getChildren().addAll(usernameLabel,  passwordLabel, usernameText, passwordText, buttonHbox);
+        gridPane.setAlignment(Pos.CENTER);
+        gridPane.setPrefSize(500.0,500.0);
+
 
         Scene scene = new Scene(gridPane, 400,400);
         scene.getStylesheets().add("JavaFX/style.css");
         primaryStage.setScene(scene);
-        primaryStage.setResizable(false);
+        primaryStage.setResizable(true);
 
         //how to set scene icon
         //todo change the shit logo Nathan made
@@ -62,7 +83,7 @@ public class JavaFXTest extends Application {
         primaryStage.show();
     }
 
-    private void verifyPassword(String text){
+    private void verifyPassword(){
         // todo use this method to validate the password
         System.out.println("kajhflakdjf");
     }
