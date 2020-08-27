3d9a9b8cc02f7340bc7769f1cadfa1a61dc25d9e
NathanCheshire <nathan.vincent.2.718@gmail.com>
2020-08-26 20:08:28 -0500

    moving label

--- src/Main.java
+++ src/Main.java
@@ -22,10 +22,39 @@ public class Main {
         JLabel desc = new JLabel("<html>An email client by <br/>Mallory Duke and<br/>Nathan Cheshire</html>");
         desc.setForeground(new Color(252, 251, 227));
         desc.setFont(new Font("Segoe UI Black", Font.BOLD, 30));
-        desc.setBounds(60,60, 800 - 60 - 60, 400);
+        desc.setBounds(20,60, 800 - 60 - 60, 400);
         sf.getContentPane().add(desc);
 
         sf.setVisible(true);
         sf.setLocationRelativeTo(null);
+
+        new Thread(() -> {
+            boolean left = false;
+            while (true) {
+                if (left) {
+                    for (int i = desc.getX() ; i < 800 - desc.getX() ; i+=1) {
+                        desc.setBounds(i,60, 800 - 60 - 60, 400);
+                        try {
+                            Thread.sleep(5);
+                        }
+
+                        catch (Exception ignored) {}
+                    }
+                    left = !left;
+                }
+
+                else {
+                    for (int i = desc.getX() ; i > 20 ; i-=1) {
+                        desc.setBounds(i,60, 800 - 60 - 60, 400);
+                        try {
+                            Thread.sleep(5);
+                        }
+
+                        catch (Exception ignored) {}
+                    }
+                    left = !left;
+                }
+            }
+        }).start();
     }
 }
