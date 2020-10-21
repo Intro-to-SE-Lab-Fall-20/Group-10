6dd98c656691f899a5bef5b0b923143cb09e1a16
Mallory Duke <69919745+malloryDuke@users.noreply.github.com>
2020-10-21 18:33:49 -0500

    Travis CI stuff again

---
+++ src/ThemeUtils/ThemeUtil.java
@@ -0,0 +1,46 @@
+package ThemeUtils;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.FileReader;
+import java.io.Reader;
+
+public class ThemeUtil {
+
+    public  String getUserTheme(String username) {
+        String theme = "DefaultStyle";
+        JSONParser parser = new JSONParser();
+        String name;
+
+        try (Reader reader = new FileReader("users.txt")) {
+            JSONObject users = (JSONObject) parser.parse(reader);
+            JSONArray array = (JSONArray) users.get("users");
+            for (Object o : array) {
+                JSONObject user = (JSONObject) o;
+                name = String.valueOf(user.get("username"));
+                if (name.equals(username)) {
+                    theme = getThemeFileString(user.get("theme").toString());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return theme;
+    }
+
+    private String getThemeFileString(String theme) {
+        switch (theme) {
+            case "PinkStyle":
+                return "PinkStyle.css";
+            case "BlueStyle":
+                return "BlueStyle.css";
+            default:
+                    return "DefaultStyle.css";
+        }
+    }
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+module Group {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.base;
+    requires javafx.web;
+    requires java.desktop;
+    requires java.mail;
+    requires java.base;
+}
