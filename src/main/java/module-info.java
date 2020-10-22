e1b2608cc51db44b26d7cd8bf29e8f8004510040
NathanCheshire <nathan.vincent.2.718@gmail.com>
2020-10-21 22:28:01 -0500

    Revert "Revert "Merge remote-tracking branch 'origin/master'""
    This reverts commit aeff68a7503d5b0cec616cdbe9956a890eb8c443.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module Group {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.base;
+    requires javafx.web;
+    requires java.desktop;
+    requires java.mail;
+    requires java.base;
+    requires json.simple;
+}
